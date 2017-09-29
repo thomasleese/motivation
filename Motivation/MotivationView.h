a96912910067171c359d69e8c5c22265d012d114
Thomas Leese <thomas@leese.io>
2017-09-29 16:21:14 +0100

    Add default screensaver

---
+++ Motivation/MotivationView.h
@@ -0,0 +1,13 @@
+//
+//  MotivationView.h
+//  Motivation
+//
+//  Created by Thomas Leese on 29/09/2017.
+//  Copyright © 2017 Thomas Leese. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface MotivationView : ScreenSaverView
+
+@end
---
+++ Motivation/MotivationView.m
@@ -0,0 +1,52 @@
+//
+//  MotivationView.m
+//  Motivation
+//
+//  Created by Thomas Leese on 29/09/2017.
+//  Copyright © 2017 Thomas Leese. All rights reserved.
+//
+
+#import "MotivationView.h"
+
+@implementation MotivationView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
